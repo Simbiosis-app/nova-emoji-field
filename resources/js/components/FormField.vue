b68f77839309fcc4cdf9c6b9cf435e4b6715eb60
Alejandro Treviño <alex_tf_1992@live.com.mx>
2022-10-18 23:23:54 -0500

    wip

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -15,8 +15,8 @@
           style="padding-right: 1px"
         >
           <span v-if="value === ''" class="flex">
-            <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 text-80" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636" />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-80">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z" />
             </svg>
           </span>
 
@@ -34,6 +34,7 @@
 <script>
 import { createPopup } from '@picmo/popup-picker'
 import { FormField, HandlesValidationErrors } from 'laravel-nova'
+import { autoTheme } from 'picmo';
 
 export default {
   mixins: [FormField, HandlesValidationErrors],
@@ -45,7 +46,7 @@ export default {
   mounted() {
     this.popupPicker = createPopup({
       autofocus: 'auto',
-      theme: 'autoTheme',
+      theme: autoTheme,
     }, {
       referenceElement: this.$refs.button,
       triggerElement: this.$refs.button,
